f0074ce3057f3f0d9dfd0ccece6508d66dab2ca4
angular <angular.user@gmail.com>
2015-07-04 15:17:43 +0800

    Added the VS Project

---
+++ BusinessAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BusinessAPI.Startup))]
+namespace BusinessAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
